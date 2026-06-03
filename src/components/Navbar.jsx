74cafe90ac570e8024418ed7e562bc02350e0ab1
Sevin Kawsika <saween.kausika123@gmail.com>
2026-06-03 15:45:44 +0530

    complete the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,30 @@
+import dayjs from "dayjs";
+import { navIcons, navLinks } from "../constants";
+
 const Navbar = () => {
     return <nav>
         <div>
             <img src="/images/logo.svg" alt="logo" />
+            <p className="font-bold">Sevin's Portfolio</p>
+
+            <ul>
+                {navLinks.map(({ id, name }) => (
+                    <li key={id}>
+                        <p>{name}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+        <div>
+            <ul>
+                {navIcons.map(({ id, img }) => (
+                    <li key={id}>
+                        <img src={img} className="icon-hover"
+                            alt={`Icon-${id}`} />
+                    </li>
+                ))}
+            </ul>
+            <time>{dayjs().format('ddd MMM D h:mm A')}</time>
         </div>
     </nav>
 };
